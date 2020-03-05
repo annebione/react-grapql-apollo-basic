498c73ebd86024a5830a72246e9518902dce7c8c
Anne Bione <anne.bione@csktech.com.br>
2020-03-05 05:10:24 -0300

    Added details component for case and contact

---
+++ src/components/Case.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+export default class Case extends Component {
+  render() {
+    let title = this.props.title;
+    let description = this.props.description;
+    let value = this.props.value;
+    let court_date = this.props.court_date;
+    let contacts = this.props.contacts;
+
+    return (
+      <React.Fragment>
+        <Link className="no-underline ma1" to={`/cases/${this.props.id}`}>
+          <h2 className="f3 black-80 fw4 lh-solid">{title}</h2>
+        </Link>
+        <p className="f3 black-80 fw4 lh-solid">{description}</p>
+        <p className="f3 black-80 fw4 lh-solid">$ {value}</p>
+        <p className="f3 black-80 fw4 lh-solid">Court Date: {court_date}</p>
+        { contacts.length > 0  ?
+            contacts.map(contact => ( 
+              <React.Fragment>
+                <h5 className="f3 black-80 fw4 lh-solid">Contacts:</h5>
+                <p className="f3 black-80 fw4 lh-solid">{contact.first_name} </p>
+                <p className="f3 black-80 fw4 lh-solid">{contact.last_name}</p>
+                <p className="f3 black-80 fw4 lh-solid">{contact.email}</p>
+                <p className="f3 black-80 fw4 lh-solid">{contact.case_role}</p>
+              </React.Fragment>
+            ))
+        : null }
+      </React.Fragment>
+    )
+  }
+}
---
+++ src/components/Contact.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+export default class Contact extends Component {
+  render() {
+    let firstName = this.props.first_name;
+    let lastName = this.props.last_name;
+    let caseRole = this.props.case_role;
+    let email  = this.props.email;
+    let cases = this.props.cases;
+
+    return (
+      <React.Fragment>
+        <Link className="no-underline ma1" to={`/contact/${this.props.id}`}>
+          <h2 className="f3 black-80 fw4 lh-solid">{title}</h2>
+        </Link>
+        <p className="f3 black-80 fw4 lh-solid">{firstName} {lastName}</p>
+        <p className="f3 black-80 fw4 lh-solid">{caseRole}</p>
+        <p className="f3 black-80 fw4 lh-solid">Contact: {email}</p>
+        { cases.length > 0  ?
+            cases.map(singleCase => ( 
+              <React.Fragment>
+                <h5 className="f3 black-80 fw4 lh-solid">Cases:</h5>
+                <Link className="no-underline ma1" to={`/cases/${singleCase.id}`}>
+                    <p className="f3 black-80 fw4 lh-solid">{singleCase.title} </p>
+                </Link>
+              </React.Fragment>
+            ))
+        : null }
+      </React.Fragment>
+    )
+  }
+}
